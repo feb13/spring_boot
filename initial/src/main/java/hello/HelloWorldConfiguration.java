93ed9889bd2433646e8b07961e412dda665b1496
Amy Sun <amysun2000@gmail.com>
2018-02-15 22:53:25 -0500

    change comment for main method.

--- initial/src/main/java/hello/HelloWorldConfiguration.java
+++ initial/src/main/java/hello/HelloWorldConfiguration.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldConfiguration {
 
 //the only java file with main method
+
 //comment for branch master
+
+//second line of comment
+
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldConfiguration.class, args);
